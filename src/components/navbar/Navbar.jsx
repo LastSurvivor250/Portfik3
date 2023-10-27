e23e3ebdd7037f37ff9662fe90bea674a7b12e94
LastSurvivor250 <45216830+LastSurvivor250@users.noreply.github.com>
2023-10-27 19:31:01 +0200

    cursor

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -9,7 +9,7 @@ const Navbar = ({ open, setOpen }) => {
       <Sidebar open={open} />
 
       <div className="wrapper">
-        CONTENT CULTURE PORTFOLIO
+        PORTFOLIO
         <motion.span
           initial={{ opacity: 0, scale: 0.5 }}
           animate={{ opacity: 1, scale: 1 }}
