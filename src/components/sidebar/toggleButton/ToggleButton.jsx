5422ee77f28d7ce3eed6d84caf9fd6468af7ed64
LastSurvivor250 <45216830+LastSurvivor250@users.noreply.github.com>
2023-10-27 10:51:43 +0200

    cursor

--- src/components/sidebar/toggleButton/ToggleButton.jsx
+++ src/components/sidebar/toggleButton/ToggleButton.jsx
@@ -1,6 +1,7 @@
+import React from "react";
 import { motion } from "framer-motion";
 
-const ToggleButton = ({ setOpen }) => {
+const ToggleButton = ({ open, setOpen }) => {
   return (
     <button onClick={() => setOpen((prev) => !prev)}>
       <svg width="23" height="23" viewBox="0 0 23 23">
@@ -12,16 +13,16 @@ const ToggleButton = ({ setOpen }) => {
             closed: { d: "M 2 2.5 L 20 2.5" },
             open: { d: "M 3 16.5 L 17 2.5" },
           }}
+          initial={open ? "open" : "closed"} // Set the initial state based on the 'open' prop
+          animate={open ? "open" : "closed"} // Animate based on the 'open' prop
         />
         <motion.path
           strokeWidth="3"
           stroke="black"
           strokeLinecap="round"
           d="M 2 9.423 L 20 9.423"
-          variants={{
-            closed: { opacity: 1 },
-            open: { opacity: 0 },
-          }}
+          initial={open ? "closed" : "open"} // Set the initial state based on the 'open' prop
+          animate={open ? "closed" : "open"} // Animate based on the 'open' prop
         />
         <motion.path
           strokeWidth="3"
@@ -31,6 +32,8 @@ const ToggleButton = ({ setOpen }) => {
             closed: { d: "M 2 16.346 L 20 16.346" },
             open: { d: "M 3 2.5 L 17 16.346" },
           }}
+          initial={open ? "open" : "closed"} // Set the initial state based on the 'open' prop
+          animate={open ? "open" : "closed"} // Animate based on the 'open' prop
         />
       </svg>
     </button>
