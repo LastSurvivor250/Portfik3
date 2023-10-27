f15a445a63eef0650215fed36f5b2a7b2d64a68e
LastSurvivor250 <45216830+LastSurvivor250@users.noreply.github.com>
2023-10-27 19:07:50 +0200

    cursor

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -48,9 +48,9 @@ const Hero = ({ open }) => {
           animate="animate"
         >
           <motion.h2 variants={textVariants}>PAUL MIGASIUK</motion.h2>
-          <motion.h1 variants={textVariants}>
-            Web Development Digital Storytelling{" "}
-          </motion.h1>
+          <motion.h2 variants={textVariants}>
+            Web Wizardry Digital Storytelling{" "}
+          </motion.h2>
           <motion.div variants={textVariants} className="buttons">
             <motion.button variants={textVariants}>
               See the Latest Works
