da20738572011cd30fd1af7800d3238973328124
LastSurvivor250 <45216830+LastSurvivor250@users.noreply.github.com>
2023-11-14 01:21:06 +0100

    mainpost

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -27,6 +27,12 @@ const items = [
     img: "https://images.pexels.com/photos/18540208/pexels-photo-18540208/free-photo-of-wood-landscape-water-hill.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
     desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores ab id ad nesciunt quo aut corporis modi? Voluptate, quos sunt dolorum facilis, id eum sequi placeat accusantium saepe eos laborum.",
   },
+  {
+    id: 5,
+    title: "Super Secret Project",
+    img: "https://www.pexels.com/pl-pl/zdjecie/jasny-miasto-ludzie-sztuka-18968224/",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores ab id ad nesciunt quo aut corporis modi? Voluptate, quos sunt dolorum facilis, id eum sequi placeat accusantium saepe eos laborum.",
+  },
 ];
 
 const Single = ({ item }) => {
