ee65e9928c9447c080e55d6c852bb65ab5f7c1b8
LastSurvivor250 <45216830+LastSurvivor250@users.noreply.github.com>
2023-11-16 23:18:33 +0100

    sdf

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -87,10 +87,12 @@ const Hero = ({ open }) => {
           <div className="hero2">
             <motion.h2 variants={textVariants}>Content Creation</motion.h2>
             <motion.h1 variants={textVariants} animate="animateFast ">
-             <p>
-              We Help Your Business Explore The Depths of Modern Internet and Find Most Effective Ways
-              to Reach Your Potential Client in Both Conventional and Unconventional Ways.
-             </p>
+              <p>
+                We Help Your Business Explore The Depths of Modern Internet and
+                Find Most Effective Ways to Reach Your Potential Client in Both
+                Conventional and Unconventional Ways.
+              </p>
+            </motion.h1>
             <motion.div variants={textVariants} className="triangle-container">
               <motion.button
                 variants={textVariants}
